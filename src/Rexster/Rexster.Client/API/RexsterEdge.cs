c97ad60c030acb001ad5a315070a26788b46fe0d
Tomas Bosak <bosak.tomas@gmail.com>
2013-01-17 13:25:13 +0100

    Implement classes for vertices and edges.

---
+++ src/Rexster/Rexster.Client/API/RexsterEdge.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Rexster.Client
+{
+    public class RexsterEdge
+    {
+        public long ID { get; set; }
+        public RexsterVertex InVertex { get; set; }
+        public RexsterVertex OutVertex { get; set; }
+        public string Label { get; set; }
+        public Dictionary<string, object> Properties { get; set; }
+
+        public RexsterEdge()
+        {
+            Properties = new Dictionary<string, object>();
+        }
+    }
+}
---
+++ src/Rexster/Rexster.Client/API/RexsterVertex.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Rexster.Client
+{
+    public class RexsterVertex
+    {
+        public long ID { get; set; }
+        public List<RexsterEdge> InEdges { get; set; }
+        public List<RexsterEdge> OutEdges { get; set; }
+        public Dictionary<string, object> Properties { get; set; }
+
+        public RexsterVertex()
+        {
+            Properties = new Dictionary<string, object>();
+        }
+    }
+}
